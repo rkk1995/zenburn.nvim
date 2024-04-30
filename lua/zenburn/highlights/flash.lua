04ae0bd40789e19699f8ba1c678f0efeea4c5ca7
Ravindu Kodippili <theravindu@gmail.com>
2024-04-30 00:43:31 -0400

    update colors

--- lua/zenburn/highlights/flash.lua
+++ lua/zenburn/highlights/flash.lua
@@ -1,9 +1,10 @@
 local c = require("zenburn.palette")
 return {
 	FlashBackdrop = c.Comment,
-	FlashMatch = { fg = c.Cursor.bg, bg = c.Macro.fg },
-	FlashCurrent = { fg = c.Cursor.fg, bg = c.IncSearch.bg },
-	FlashLabel = { fg = c.Cursor.fg, bg = c.Identifier.fg },
+	FlashCurrent = { fg = c.Cursor.fg, bg = "#f4c067" },
+	-- FlashCurrent = { fg = c.Cursor.fg, bg = c.Todo.fg, bold = true, underline = true },
+	FlashMatch = { fg = c.Cursor.fg, bg = c.Identifier.fg },
+	FlashLabel = { fg = c.Cursor.fg, bg = c.IncSearch.bg },
 	FlashPrompt = { fg = c.Cursor.fg, bg = c.Identifier.fg },
 	FlashPromptIcon = { fg = c.Cursor.fg, bg = c.Identifier.fg },
 	FlashCursor = c.Cursor,
